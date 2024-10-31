128cd467324df53a4a3d8dee035624dc4043d159
emmanuelunung <eunung2003@gmail.com>
2024-10-31 16:19:42 +0100

    modified requirements.txt

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -13,6 +13,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,6 +84,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES["default"] = dj_database_url.parse(os.getenv('DATABASE_URL'))
 
 
 # Password validation
